e84a25951f758ed25f0e11e30250d03836cc89b0
Felipe Díaz de Burgos <felipe@lean2tech.com>
2021-04-30 18:42:33 +0200

    M2 Wireframe a HTML N2 y N3

---
+++ scripts/footer.php
@@ -0,0 +1,12 @@
+<?php
+    echo "
+    <div class="footer-container">
+        <div>
+            <p>&copy; HC Gourmet 2021</p>
+        </div>
+        <div>
+            <p>Tfno: 699999999</p>
+            <p>Direcci&oacute;n: Xxxxxx</p>
+        </div>
+    </div>";
+?>
